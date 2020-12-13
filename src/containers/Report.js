40fad70f277e481313d06b0690679dcd89ae5e7d
Rakesh Buru <beat@Rakeshs-MacBook-Air.local>
2020-12-13 16:08:11 +0530

    intial commit

--- src/containers/Report.js
+++ src/containers/Report.js
@@ -1,86 +1,86 @@
-import React from 'react';
-import Page from 'src/components/Page';
-import ReportData from '../components/report/ReportComponent';
-import { jsPDF } from 'jspdf';
-import autoTable from 'jspdf-autotable';
-import axios from 'axios';
-import { makeStyles, Grid } from '@material-ui/core';
+import React from "react";
+import Page from "src/components/Page";
+import ReportData from "../components/report/ReportComponent";
+import { jsPDF } from "jspdf";
+import autoTable from "jspdf-autotable";
+import axios from "axios";
+import { makeStyles, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
-    margin: 5
+    margin: 5,
   },
   table: {
-    minWidth: 650
-  }
+    minWidth: 650,
+  },
 });
 
 const headers = [
-  { label: 'Activity Date', key: 'activity' },
-  { label: 'Customer', key: 'calories' },
-  { label: 'Product', key: 'fat' },
-  { label: 'Memo', key: 'carbs' },
-  { label: 'Duration', key: 'protein' },
-  { label: 'Billable', key: 'billable' }
+  { label: "Activity Date", key: "activity" },
+  { label: "Customer", key: "calories" },
+  { label: "Product", key: "fat" },
+  { label: "Memo", key: "carbs" },
+  { label: "Duration", key: "protein" },
+  { label: "Billable", key: "billable" },
 ];
 
 const rows = [
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:No of Hours Worked',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:No of Hours Worked",
+    carbs: "student moduke",
     protein: 4.0,
-    billable: 'Yes'
+    billable: "Yes",
   },
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:UI Services',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:UI Services",
+    carbs: "student moduke",
     protein: 6.0,
-    billable: 'Yes'
+    billable: "Yes",
   },
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:No of Hours Worked',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:No of Hours Worked",
+    carbs: "student moduke",
     protein: 9.0,
-    billable: 'No'
+    billable: "No",
   },
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:UI Services',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:UI Services",
+    carbs: "student moduke",
     protein: 3.0,
-    billable: 'Yes'
+    billable: "Yes",
   },
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:UI Services',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:UI Services",
+    carbs: "student moduke",
     protein: 7.0,
-    billable: 'No'
+    billable: "No",
   },
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:UI Services',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:UI Services",
+    carbs: "student moduke",
     protein: 7.0,
-    billable: 'No'
+    billable: "No",
   },
   {
-    activity: '11/01/2020',
-    calories: 'Cosmas Majachani',
-    fat: 'Off-Shore:UI Services',
-    carbs: 'student moduke',
+    activity: "11/01/2020",
+    calories: "Cosmas Majachani",
+    fat: "Off-Shore:UI Services",
+    carbs: "student moduke",
     protein: 7.0,
-    billable: 'No'
-  }
+    billable: "No",
+  },
 ];
 
 export default function Report() {
@@ -90,10 +90,10 @@ export default function Report() {
 
   const getReport = () => {
     axios
-      .get('/user', {
+      .get("/user", {
         params: {
-          ID: 12345
-        }
+          ID: 12345,
+        },
       })
       .then(function(response) {
         console.log(response);
@@ -105,20 +105,20 @@ export default function Report() {
   };
 
   React.useEffect(() => {
-    console.log('Run useEffect');
+    console.log("Run useEffect");
     getReport();
   }, []);
 
   const exportAsPdf = () => {
     var doc = new jsPDF();
-    autoTable(doc, { html: '#table-data' });
-    doc.save('table.pdf');
+    autoTable(doc, { html: "#table-data" });
+    doc.save("table.pdf");
   };
   const printTable = () => {
-    var content = document.getElementById('table-data-container');
-    var pri = document.getElementById('ifmcontentstoprint').contentWindow;
+    var content = document.getElementById("table-data-container");
+    var pri = document.getElementById("ifmcontentstoprint").contentWindow;
     pri.document.open();
-    console.log('content', content.innerHTML);
+    console.log("content", content.innerHTML);
     pri.document.write(content.innerHTML);
     pri.document.close();
     pri.focus();
@@ -128,7 +128,7 @@ export default function Report() {
     <Page className={classes.root} title="Report">
       <iframe
         id="ifmcontentstoprint"
-        style={{ height: '0px', width: '0px', position: 'absolute' }}
+        style={{ height: "0px", width: "0px", position: "absolute" }}
       ></iframe>
       <div id="test-test"></div>
       <Grid item xs={12}>
